03dd428c1f4be26279302f142e2e52208538f339
Stéphane Denis <sdenis@cegepsth.qc.ca>
2022-10-17 12:47:56 -0400

    Code généré avec l'application à partir de la dernière version sur le site eclipse

---
+++ docs/EPF-PracticeLib/scripts/processElementData.js
@@ -0,0 +1,27 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["_SKnA1uL9EdyM47cGD2jiaQActivity"]="./../../practice.mgmt.risk_value_lifecycle.base/capabilitypatterns/resources/risk_value_lifecycle_phases_4D57D552_4d57d552_Activity.jpeg"
+contentPage.processPage.elementUrls["_SKnAzuL9EdyM47cGD2jiaQ"] = ["Inception Iteration [1..n]","practice.mgmt.risk_value_lifecycle.base/capabilitypatterns/inception_iteration_5AF15029.html"];
+contentPage.processPage.elementUrls["_ePfxAFTiEd2ZeY-22kx0mw"] = ["Initial Operational Capability Milestone","practice.mgmt.risk_value_lifecycle.base/capabilitypatterns/Initial Operational Capability_D87619F1.html"];
+contentPage.processPage.elementUrls["__r658VThEd2ZeY-22kx0mw"] = ["Construction Phase","practice.mgmt.risk_value_lifecycle.base/capabilitypatterns/construction_phase_E35610BE.html"];
+contentPage.processPage.elementUrls["_SKnA0uL9EdyM47cGD2jiaQ"] = ["Elaboration Iteration [1..n]","practice.mgmt.risk_value_lifecycle.base/capabilitypatterns/elaboration_iteration_88C5F333.html"];
+contentPage.processPage.elementUrls["_XJ0u0FTiEd2ZeY-22kx0mw"] = ["Lifecycle Architecture Milestone","practice.mgmt.risk_value_lifecycle.base/capabilitypatterns/Lifecycle Architecture Milestone_65437D7C.html"];
+contentPage.processPage.elementUrls["_TUrpQFTiEd2ZeY-22kx0mw"] = ["Lifecycle Objectives Milestone","practice.mgmt.risk_value_lifecycle.base/capabilitypatterns/Lifecycle Objectives Milestone_DFF2A143.html"];
+contentPage.processPage.elementUrls["_nm0swFTiEd2ZeY-22kx0mw"] = ["Product Release Milestone","practice.mgmt.risk_value_lifecycle.base/capabilitypatterns/Product Release Milestone_96CC0AE0.html"];
+contentPage.processPage.elementUrls["_BBN48VTiEd2ZeY-22kx0mw"] = ["Transition Phase","practice.mgmt.risk_value_lifecycle.base/capabilitypatterns/transition_phase_8CDF05F3.html"];
+contentPage.processPage.elementUrls["_SKnAxOL9EdyM47cGD2jiaQ"] = ["Transition Iteration [1..n]","practice.mgmt.risk_value_lifecycle.base/capabilitypatterns/transition_iteration_D89697C5.html"];
+contentPage.processPage.elementUrls["_SKnAyOL9EdyM47cGD2jiaQ"] = ["Construction Iteration [1..n]","practice.mgmt.risk_value_lifecycle.base/capabilitypatterns/construction_iteration_9D2879E4.html"];
+contentPage.processPage.elementUrls["_2_nRIVThEd2ZeY-22kx0mw"] = ["Elaboration Phase","practice.mgmt.risk_value_lifecycle.base/capabilitypatterns/elaboration_phase_53DE05EC.html"];
+contentPage.processPage.elementUrls["_SKnA1uL9EdyM47cGD2jiaQ"] = ["Risk-Value Lifecycle Phases","practice.mgmt.risk_value_lifecycle.base/capabilitypatterns/risk_value_lifecycle_phases_4D57D552.html"];
+contentPage.processPage.elementUrls["_-UFkUVThEd2ZeY-22kx0mw"] = ["Inception Phase","practice.mgmt.risk_value_lifecycle.base/capabilitypatterns/inception_phase_5078703C.html"];
